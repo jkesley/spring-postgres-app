7285d5e2d0f2b428bb2902cfe20e98c1c66b0a49
jkesley <jkesley@gmail.com>
2025-08-13 15:39:10 -0300

    corrigindo para a jdk 17, por causa de incompatibilidade das novas vercões

--- src/main/java/com/alunoskesley/professorkesley/ProfessorkesleyApplication.java
+++ src/main/java/com/alunoskesley/professorkesley/ProfessorkesleyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfessorkesleyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorkesleyApplication.class, args);
 	}
-
 }
